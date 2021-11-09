1a9a523936910ec6fe03a4b8d825b6507e2871d0
James <jguest777@gmail.com>
2021-11-08 21:01:04 -0500

    parallax footer

--- src/App/App.js
+++ src/App/App.js
@@ -96,17 +96,19 @@ function App() {
           )}
         </div>
       )}
-
-      <footer className="footer">
-        <div className="footer__mini">
-          <p>
-            &copy; 2021 <span>BioPlus Theme</span> by James Guest
-          </p>
-          <ul>
-            <li>Home</li>
-            <li>About Us</li>
-            <li>Contact</li>
-          </ul>
+      <div className="parallax" />
+      <footer>
+        <div className="footer-container">
+          <div className="footer-inner">
+            <p>
+              &copy; 2021 <span>BioPlus Theme</span> by James Guest
+            </p>
+            <ul>
+              <li>Home</li>
+              <li>About Us</li>
+              <li>Contact</li>
+            </ul>
+          </div>
         </div>
       </footer>
     </div>
